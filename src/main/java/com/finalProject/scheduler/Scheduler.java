1f297929c4bd5e9b8e427b5790ba43bd1eb7f3bf
Hasoo99 <fhtkwhgdk99@gmail.com>
2024-11-14 14:13:24 +0900

    저장

--- src/main/java/com/finalProject/scheduler/Scheduler.java
+++ src/main/java/com/finalProject/scheduler/Scheduler.java
@@ -14,23 +14,24 @@ public class Scheduler {
 	@Autowired
 	private MemberService memberService;
 
-	@Scheduled(cron = "30 * * * * *") // 30초마다
-	public void performTask() {
-		try {
-			int count = memberService.getMemberCount(); // 회원 수 저장
-			System.out.println("회원 수 : " + count);
-			// 회원 수 만큼 반복
-			for (int i = 0; i < count; i++) {
-				String member_id = memberService.getMemberId(i); // 회원 아이디 받기
-				int totalPrice = memberService.getTotalPriceByMemberId(member_id); // 회원의 지난 1년간 결제금액 조회
-				int result = memberService.updateMemberLevel(member_id, totalPrice); // 회원 등급 업데이트
-				System.out.println("회원 아이디 : " + member_id + ", 사용금액 : " + totalPrice + ", update결과 : " + result);
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+//	@Scheduled(cron = "30 * * * * *") // 30초마다
+//	public void performTask() {
+//		try {
+//			int count = memberService.getMemberCount(); // 회원 수 저장
+//			System.out.println("회원 수 : " + count);
+//			// 회원 수 만큼 반복
+//			for (int i = 0; i < count; i++) {
+//				String member_id = memberService.getMemberId(i); // 회원 아이디 받기
+//				int totalPrice = memberService.getTotalPriceByMemberId(member_id); // 회원의 지난 1년간 결제금액 조회
+//				int result = memberService.updateMemberLevel(member_id, totalPrice); // 회원 등급 업데이트
+//				System.out.println("회원 아이디 : " + member_id + ", 사용금액 : " + totalPrice + ", update결과 : " + result);
+//			}
+//			System.out.println("갱신 완료");
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
 
 	@Scheduled(cron = "0 0 0 1 1/3 ?") // (0초, 0분, 0시, 1일, 1월/3개월마다, 요일상관없음)
 	public void updateMembersLevel() {
@@ -49,7 +50,7 @@ public class Scheduler {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Scheduled(cron = "0 0/30 * 1/1 * ?") // (매일 30분마다 동작)
 	public void test() {
 		try {
