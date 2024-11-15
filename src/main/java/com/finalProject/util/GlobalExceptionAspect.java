e5569f615525d7a1ce27427790f7b2ecd1eeef4b
rutokki <rlawnsqja970@naver.com>
2024-11-16 02:21:27 +0900

    1
    1

--- src/main/java/com/finalProject/util/GlobalExceptionAspect.java
+++ src/main/java/com/finalProject/util/GlobalExceptionAspect.java
@@ -1,18 +1,16 @@
-package com.finalProject.util;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
-
-@ControllerAdvice
-public class GlobalExceptionAspect {
-
-	@ExceptionHandler(Exception.class)
-    public String handleException(Exception e) {
-		System.out.println("Exception 발생");
-//        ModelAndView mv = new ModelAndView();
-//        mv.setViewName("/admin/coupon/coupons");  // 리다이렉트할 URL 설정
-        return "/user/pages/warning";
-        
-    }
-}
+/*
+ * package com.finalProject.util;
+ * 
+ * import org.springframework.web.bind.annotation.ControllerAdvice; import
+ * org.springframework.web.bind.annotation.ExceptionHandler; import
+ * org.springframework.web.servlet.ModelAndView;
+ * 
+ * @ControllerAdvice public class GlobalExceptionAspect {
+ * 
+ * @ExceptionHandler(Exception.class) public String handleException(Exception e)
+ * { System.out.println("Exception 발생"); // ModelAndView mv = new
+ * ModelAndView(); // mv.setViewName("/admin/coupon/coupons"); // 리다이렉트할 URL 설정
+ * return "/user/pages/warning";
+ * 
+ * } }
+ */
